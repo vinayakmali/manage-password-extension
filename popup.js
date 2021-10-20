995677bccdf4029d7556e219a5b5bcb7d925a4ca
Vinayak Mali <vinayak.mali@harbingergroup.com>
2021-10-20 12:41:13 +0530

    Change in code

--- popup.js
+++ popup.js
@@ -39,6 +39,10 @@ var queryInfo = {
     "email": email,
     "login_url": url
     });
+if (email == '') {
+       document.getElementById("error").innerHTML = email+"sdsds";
+       return false;
+}
 
 
 
@@ -63,6 +67,7 @@ xhr.addEventListener("readystatechange", function() {
       chrome.tabs.executeScript(null,{code:"var form = document.querySelector('input[type=password]').parentNode.parentElement;form.querySelector('input[type=email]').value = '"+username+"'"});
       chrome.tabs.executeScript(null,{code:"var form = document.querySelector('input[type=password]').parentNode.parentElement.parentElement;form.querySelector('input[type=email]').value = '"+username+"'"});
       chrome.tabs.executeScript(null,{code:"var form = document.querySelector('input[type=password]').parentNode.parentElement.parentElement;form.querySelector('input[type=text]').value = '"+username+"'"});
+      document.getElementById("success").innerHTML = 'Success';
 
     }else{
       document.getElementById("error").innerHTML = obj.result;
@@ -70,12 +75,9 @@ xhr.addEventListener("readystatechange", function() {
 
   }
 });
-
-xhr.open("POST", "http://127.0.0.1:8000/getpassword");
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("Cookie", "XSRF-TOKEN=eyJpdiI6IkFEYnhFQnZic0Z1NWNZQnNQSnZrWUE9PSIsInZhbHVlIjoia2JxVWxiSDE4K1RjdXVUd1J3Z0ZxVEd6R0ZzeGZsNkh0L05yV2ZWVzV0MTBSUlNqQkM2Ryt0ZVg4TklPb0cwZU9WOFI3T1h4NWVqZTBCSi9YdVp5UU41b0xmbnRLQmZNN1J0b3V2WEhHZFZZUTc5bGhFaWtnWTdUV3F5bzJ5Q3AiLCJtYWMiOiJiODhiMTE5YjliZGI5YWIxOTZhNDI2ZDY1MzY2Y2YwYmQ4MDY5MDVjYjU2MGVhOTViMDJhZDFhMGExZWFhODMxIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IjErYldpelg3VEQ4RCtzZk8zYkhEdmc9PSIsInZhbHVlIjoiWEFCVUsxTlFmSFZwQ3NFSm14K3ViS2s5U0ZvRWFjOHducGEzUzA4UFNZcGNCRnVWVUpHSDA0MDRvMlRhRlZRQ1JZbytnTXcyd3oralQvS1p6QjZ0aXNKOVZLTjNyTVRYRjNycHZEaVdUN2FlWG9sU0g5R2MyLzRURmRjQ0pMMHkiLCJtYWMiOiIwZDIxZTYwMDE5OWFlMjdhYjJlMWY4MDExMjQ5YmEzN2JkZTFmNmRiYjQ3ZjgwODc5ODBkMTZhZDJlOWE3MjYzIiwidGFnIjoiIn0%3D");
-
-xhr.send(data);
+xhr.open("GET", "http://127.0.0.1:8000/api/getpassword?email="+ email +"&login_url="+ url);
+xhr.setRequestHeader("Authorization", "Basic " + btoa(email+":1Externaluser@23"));
+xhr.send();
 
 
 });
